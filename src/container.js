c486664a70f14b9b125d72b24f868caba2c06fb1
Thomas Hooper <tom@stainlessvision.com>
2017-10-21 15:11:13 +0200

    Keep icosahedron faces flat

--- src/container.js
+++ src/container.js
@@ -8,10 +8,13 @@ const Container = function(parent, app) {
     parent.add(group);
 
     const geometry = new THREE.IcosahedronGeometry(1, 1);
+    geometry.computeFlatVertexNormals();
+
     const material = new THREE.MeshBasicMaterial({
         color: 0x000000
     });
     material.wireframe = true;
+
     const mesh = new THREE.Mesh(geometry, material);
 
     group.add(mesh);
