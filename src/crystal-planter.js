7c4ee24d5e2c42b8371c2b792934fe1cdf1d283a
Thomas Hooper <tom@stainlessvision.com>
2017-10-26 21:10:10 +0200

    Stop crystals growing over eachother

--- src/crystal-planter.js
+++ src/crystal-planter.js
@@ -1,25 +1,25 @@
-var Crystal = require('./crystal');
+const THREE = require('three');
+
+const Crystal = require('./crystal');
 
 
 const CrystalPlanter = function(parent, app) {
     this.parent = parent;
     this.app = app;
+    this.crystals = [];
 
     app.eventMediator.on('soil-cursor.down', this.onMouseDown.bind(this));
     app.eventMediator.on('soil-cursor.up', this.onMouseUp.bind(this));
 };
 
-
 CrystalPlanter.prototype.onMouseDown = function(intersection) {
-    var position = intersection.point.clone();
+    const position = intersection.point.clone();
     this.parent.worldToLocal(position);
-
-    var normal = intersection.normal.clone();
-    this.parent.worldToLocal(normal);
-    var top = intersection.point.clone().add(normal);
-    this.parent.worldToLocal(top);
-
-    this.activeCrystal = new Crystal(this.parent, this.app, position, top);
+    const normal = intersection.normal.clone();
+    const crystal = new Crystal(this.parent, this.app, position, normal);
+    this.crystals.push(crystal);
+    this.activeCrystal = crystal;
+    this.adjustNormals();
 };
 
 CrystalPlanter.prototype.onMouseUp = function() {
@@ -28,5 +28,43 @@ CrystalPlanter.prototype.onMouseUp = function() {
     }
 };
 
+CrystalPlanter.prototype.adjustNormals = function() {
+    const separation = .3;
+
+    this.crystals.forEach((crystalA) => {
+        this.crystals.forEach((crystalB) => {
+            if (crystalA === crystalB) {
+                return;
+            }
+            if (crystalA.idealNormals.hasOwnProperty(crystalB.id)) {
+                return;
+            }
+            var midNormal = crystalA.normal.clone().add(crystalB.normal).normalize();
+            var adjacent = crystalA.position.clone().sub(crystalB.position).normalize();
+            var tangent = midNormal.clone().cross(adjacent);
+            var idealA = midNormal.clone().applyAxisAngle(tangent, separation);
+            var idealB = midNormal.clone().applyAxisAngle(tangent, -separation);
+            var distance = crystalA.position.distanceTo(crystalB.position);
+            crystalA.idealNormals[crystalB.id] = [idealA, distance];
+            crystalB.idealNormals[crystalA.id] = [idealB, distance];
+        });
+    });
+
+    this.crystals.forEach((crystal) => {
+        var normal = crystal.normal.clone();
+        for (const [key, ideal] of Object.entries(crystal.idealNormals)) {
+            var scale = Math.max(1 - ideal[1], 0);
+            if ( ! scale) {
+                continue;
+            }
+            scale = Math.pow(scale, 2);
+            var idealNormal = ideal[0].clone();
+            idealNormal.multiplyScalar(scale);
+            normal.add(idealNormal);
+        }
+        normal.normalize();
+        crystal.setDirection(normal);
+    });
+};
 
 module.exports = CrystalPlanter;
--- src/crystal.js
+++ src/crystal.js
@@ -2,17 +2,28 @@ const THREE = require('three');
 const crystalGen = require('crystal-gen');
 
 
-const Crystal = function(parent, app, position, top) {
+const Crystal = function(parent, app, position, normal) {
+    this.idealNormals = {};
+    this.position = position;
+    this.normal = normal;
+
     const geometry = this.createGeometry();
     const material = new THREE.MeshNormalMaterial();
     const mesh = new THREE.Mesh(geometry, material);
-
     parent.add(mesh);
 
-    mesh.position.copy(position);
-    mesh.lookAt(top);
-
     this.mesh = mesh;
+    this.id = mesh.id;
+
+    mesh.position.copy(position);
+    this.setDirection(normal);
+
+    const rotation = Math.random();
+    this.mesh.up = new THREE.Vector3(
+        Math.sin(rotation),
+        0,
+        Math.cos(rotation)
+    );
 
     var TWEEN = app.TWEEN;
     this.mesh.scale.set(.25, .25, 0);
@@ -23,6 +34,10 @@ const Crystal = function(parent, app, position, top) {
             app.eventMediator.emit('crystal.growth', progress);
         })
         .start();
+};
+
+Crystal.prototype.setDirection = function(normal) {
+    this.mesh.lookAt(this.position.clone().add(normal));
 };
 
 Crystal.prototype.createGeometry = function() {
