143b9f9f27fd794e5c26ab69691ae4b5c72ca1db
Thomas Hooper <tom@stainlessvision.com>
2017-11-05 20:28:37 +0100

    wip

--- src/soil.js
+++ src/soil.js
@@ -1,5 +1,7 @@
+var cga = require('cga');
 const THREE = require('three');
 const ThreeBSP = require('./lib/three-js-csg')(THREE);
+const sliceGeometry = require('threejs-slice-geometry')(THREE);
 
 
 const Soil = function(parent, container, app) {
@@ -13,23 +15,107 @@ const Soil = function(parent, container, app) {
     this.offset = size.y * .2;
 
     const material = new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        shininess: 0
+        color: 0x000000,
+        shininess: 0,
+        wireframe: true
     });
 
-    const surface = new THREE.ParametricGeometry(
+    var sections = 5;
+
+    var surface = new THREE.ParametricGeometry(
         this.generate.bind(this),
-        25, 25
+        sections, sections
     );
 
-    const containerBSP = new ThreeBSP(container);
-    const surfaceBSP = new ThreeBSP(surface);
+    var addHelper = function(v) {
+        var g = new THREE.SphereGeometry(.05);
+        var m = new THREE.Mesh(g);
+        m.position.copy(v);
+        parent.add(m);
+    };
+
+    var res = sections + 1;
+    var bottom = -1;
+
+    surface.vertices.push(new THREE.Vector3(this.width * .5, bottom, this.depth * .5));
+    surface.vertices.push(new THREE.Vector3(this.width * -.5, bottom, this.depth * .5));
+    surface.vertices.push(new THREE.Vector3(this.width * .5, bottom, this.depth * -.5));
+    surface.vertices.push(new THREE.Vector3(this.width * -.5, bottom, this.depth * -.5));
+
+    // addHelper(surface.vertices[surface.vertices.length - 1]);
+    // addHelper(surface.vertices[surface.vertices.length - 2]);
+    // addHelper(surface.vertices[surface.vertices.length - 3]);
+    // addHelper(surface.vertices[surface.vertices.length - 4]);
+
+    // create corner vertices
+
+    // get top vertices for edge
+    // get bottom vertices
+    // create a new list of 2d vertices
+    // create a map from the 2d index to the geom vertex index
+    // triangulate
+    // for each triangle, add face using map
+
+    // -x face
+    // var sideIndices = [...Array(sections + 1)].map(function(v, i) {
+    //     return i;
+    // });
+    // sideIndices.push(surface.vertices.length - 3);
+    // sideIndices.push(surface.vertices.length - 1);
+    // this.addSideFaces(surface, sideIndices);
+
+    // +x face
+    var sideIndices = [...Array(sections + 1)].map(function(v, i) {
+        return (sections + 1) * sections + i;
+    });
+    sideIndices.push(surface.vertices.length - 4);
+    sideIndices.push(surface.vertices.length - 2);
+    // sideIndices.reverse();
+    sideIndices.push(sideIndices.pop());
+    this.addSideFaces(surface, sideIndices);
 
 
-    // Visible soil
+    sideIndices.forEach(i => {
+        var vertex = surface.vertices[i];
+        addHelper(vertex);
+    });
 
-    const geomBSP = containerBSP.intersect(surfaceBSP);
-    const geometry = geomBSP.toGeometry();
+
+
+    parent.add(new THREE.AxisHelper(1));
+
+
+    // for (var u = 0; u <= res * 2; u++) {
+    //     // for (var v = 0; v < res * 2; v++) {
+    //         var x = (((u % res) / (res - 1)) - .5) * this.width;
+    //         var z = u > res ? this.depth * .5 : this.depth * -.5;
+    //         var vertex = new THREE.Vector3(
+    //             x,-this.offset,z
+    //         );
+    //         addHelper(vertex);
+    //     // }
+    // }
+
+
+    // container.faces.forEach(face => {
+    //     var plane = new THREE.Plane().setFromCoplanarPoints(
+    //         container.vertices[face.b],
+    //         container.vertices[face.a],
+    //         container.vertices[face.c]
+    //     );
+    //     surface = sliceGeometry(surface, plane);
+    // });
+
+    // const containerBSP = new ThreeBSP(container);
+    // const surfaceBSP = new ThreeBSP(surface);
+
+
+    // // Visible soil
+
+    // const geomBSP = containerBSP.intersect(surfaceBSP);
+    // const geometry = geomBSP.toGeometry();
+
+    var geometry = surface;
     const mesh = new THREE.Mesh(geometry, material);
     mesh.receiveShadow = true;
     parent.add(mesh);
@@ -37,25 +123,52 @@ const Soil = function(parent, container, app) {
     this.mesh = mesh;
 
 
-    // Interactive area of the soil
+    // // Interactive area of the soil
 
-    var interactiveBSP = surfaceBSP.cut(containerBSP);
-    var interactiveGeom = interactiveBSP.toGeometry();
-    var interactiveMesh = new THREE.Mesh(interactiveGeom, material);
-    interactiveMesh.visible = false;
-    parent.add(interactiveMesh);
+    // var interactiveBSP = surfaceBSP.cut(containerBSP);
+    // var interactiveGeom = interactiveBSP.toGeometry();
+    // var interactiveMesh = new THREE.Mesh(interactiveGeom, material);
+    // interactiveMesh.visible = false;
+    // parent.add(interactiveMesh);
 
-    app.interactionPublisher.add(interactiveMesh, 'soil-area', true);
+    // app.interactionPublisher.add(interactiveMesh, 'soil-area', true);
 
 
-    // Clean normals for the soil
+    // // Clean normals for the soil
 
-    surface.computeFaceNormals();
-    var normalMesh = new THREE.Mesh(surface, material);
-    normalMesh.visible = false;
-    parent.add(normalMesh);
+    // surface.computeFaceNormals();
+    // var normalMesh = new THREE.Mesh(surface, material);
+    // normalMesh.visible = false;
+    // parent.add(normalMesh);
 
-    app.interactionPublisher.add(normalMesh, 'soil-normals', true);
+    // app.interactionPublisher.add(normalMesh, 'soil-normals', true);
+};
+
+Soil.prototype.addSideFaces = function(geometry, indices) {
+    var vertices2d = [];
+    var vertexMap = [];
+
+    indices.forEach(i => {
+        var vertex = geometry.vertices[i];
+        vertices2d.push([
+            vertex.z,
+            vertex.y,
+        ]);
+        vertexMap.push(i);
+    });
+
+    vertices2d.reverse();
+
+    var faces = cga.triangulatePolygon2(vertices2d)
+        .map(face => {
+            return new THREE.Face3(
+                vertexMap[face[2]],
+                vertexMap[face[1]],
+                vertexMap[face[0]]
+            );
+        });
+
+    geometry.faces = geometry.faces.concat(faces);
 };
 
 Soil.prototype.generate = function(u, v) {
