62cee1aebebadeafbccbaeff0b9dc0cabf197438
Thomas Hooper <thomash@last.fm>
2017-11-12 22:16:49 +0000

    Don't hide controls while growing

--- src/activity.js
+++ src/activity.js
@@ -2,15 +2,19 @@
 const INACTIVE_DELAY = 2500;
 
 function ActivityMonitor() {
-    this.active();
-    document.addEventListener('mousemove', this.active.bind(this));
-    document.addEventListener('mousedown', this.active.bind(this));
-    document.addEventListener('mouseup', this.active.bind(this));
-    document.addEventListener('keydown', this.active.bind(this));
-    document.addEventListener('keyup', this.active.bind(this));
-    document.addEventListener('touchstart', this.active.bind(this));
-    document.addEventListener('touchmove', this.active.bind(this));
-    document.addEventListener('touchend', this.active.bind(this));
+    this.isActive = true;
+    this.setInactive();
+
+    document.addEventListener('mousemove', this.move.bind(this));
+    document.addEventListener('touchmove', this.move.bind(this));
+
+    document.addEventListener('keydown', this.start.bind(this));
+    document.addEventListener('mousedown', this.start.bind(this));
+    document.addEventListener('touchstart', this.start.bind(this));
+
+    document.addEventListener('mouseup', this.end.bind(this));
+    document.addEventListener('touchend', this.end.bind(this));
+    document.addEventListener('keyup', this.end.bind(this));
 }
 
 ActivityMonitor.prototype.clearTimeout = function() {
@@ -20,17 +24,32 @@ ActivityMonitor.prototype.clearTimeout = function() {
     }
 };
 
-ActivityMonitor.prototype.active = function() {
+ActivityMonitor.prototype.move = function() {
+    this.end();
+};
+
+ActivityMonitor.prototype.start = function() {
     this.clearTimeout();
-    this.timeout = setTimeout(this.inactive.bind(this), INACTIVE_DELAY);
+    this.setActive();
+};
+
+ActivityMonitor.prototype.end = function() {
+    this.clearTimeout();
+    this.timeout = setTimeout(() => {
+        this.clearTimeout();
+        this.setInactive();
+    }, INACTIVE_DELAY);
+    this.setActive();
+};
+
+ActivityMonitor.prototype.setActive = function() {
     if ( ! this.isActive) {
         this.isActive = true;
         document.body.classList.remove('activity-monitor-inactive');
     }
 };
 
-ActivityMonitor.prototype.inactive = function() {
-    this.clearTimeout();
+ActivityMonitor.prototype.setInactive = function() {
     if (this.isActive) {
         this.isActive = false;
         document.body.classList.add('activity-monitor-inactive');
