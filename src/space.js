3e1f4970ee4878d64f4cf11194263c21a0eb8e24
Thomas Hooper <thomash@last.fm>
2017-11-16 21:01:34 +0000

    Animate animate planets again

--- src/space.js
+++ src/space.js
@@ -13,8 +13,7 @@ const Space = function(parent, app) {
     this.group = group;
     this.app = app;
 
-    // this.addPlanets();
-    this.test();
+    this.addPlanets();
     this.addStars();
 
     const t = {t: 0};
@@ -31,7 +30,6 @@ const Space = function(parent, app) {
 Space.prototype.setVisible = function(value) {
     this.group.visible = value;
 };
-
 
 Space.prototype.addStars = function() {
     const geometry = new THREE.BufferGeometry();
@@ -71,77 +69,51 @@ Space.prototype.addStars = function() {
 };
 
 Space.prototype.addPlanets = function() {
-    const solidMaterial = materials.soilBottom;
-    // solidMaterial.uniforms.seed.value = 3;
-    // solidMaterial.uniforms.scale.value = .2;
-    // solidMaterial.uniforms.height.value = 1;
-    // solidMaterial.uniforms.flash.value = 3;
 
-    const lineMaterial = materials.planetWireframe;
+    var bufferGeometry = new THREE.OctahedronBufferGeometry();
+    bufferGeometry.scale(.2,.2,.5);
 
-    const radius = 20;
-    const geometries = this.geometries();
-    const distributionA = new THREE.IcosahedronGeometry(radius);
-    const distributionB = new THREE.DodecahedronGeometry(radius);
+    var wireframeGeometry = new THREE.OctahedronGeometry();
+    wireframeGeometry.scale(.2,.2,.5);
+    wireframeGeometry = geometryTools.wireframeMesh(wireframeGeometry, .001);
+    var wireframeBufferGeometry = new THREE.BufferGeometry().fromGeometry(wireframeGeometry);
 
-    distributionB.rotateX(Math.PI * .5);
+    var origin = new THREE.Vector3();
+    var up = new THREE.Vector3(0,1,0);
 
-    distributionA.rotateX(1.1);
-    distributionB.rotateX(1.1);
-    // distributionA.rotateY(4.8);
-    // distributionB.rotateY(4.8);
+    var icoDistribution = new THREE.IcosahedronGeometry(1);
 
-    var verts = distributionA.vertices.concat(distributionB.vertices);
-    // var verts = distributionB.vertices;
-
-    const TWEEN = this.app.TWEEN;
-
-    verts.forEach((vert, i) => {
-        var direction, position, size, dist, geom;
-
-        if (i < distributionA.vertices.length) {
-            geom = 0;
-            size = random.floatBetween(0, 1/3);
-        } else if (i % 2 === 0){
-            geom = 1;
-            size = random.floatBetween(1/3, 2/3);
-        } else {
-            geom = 2;
-            size = random.floatBetween(2/3, 1);
-        }
-
-        // size = random.random();
-        dist = random.floatBetween(1, 1.3);
-        direction = new THREE.Vector3().fromArray(this.randomPointOnSphere(3, random.random));
-        position = vert.clone().multiplyScalar(dist).add(direction);
-        
-        const geometry = geometries[geom];
-        const rotate = THREE.Math.lerp(15000, 300000, size);
-
-        var mesh;
-
-        if (geom === 0) {
-            mesh = new THREE.Mesh(geometry.solid, solidMaterial);
-        } else {
-            mesh = new THREE.LineSegments(geometry.wireframe, lineMaterial);
-        }
-
-        size = size * 5 + 1;
-        
-        mesh.position.copy(position);
-        mesh.scale.set(size, size, size);
-        mesh.rotateX(random.random() * Math.PI * 2);
-        mesh.rotateY(random.random() * Math.PI);
-        this.group.add(mesh);
-
-        mesh.rotation.z = 0;
-        new TWEEN.Tween(mesh.rotation)
-            .to({z: Math.PI * 2}, rotate)
-            .repeat(Infinity)
-            .start();
+    var snowflakeMatricies = icoDistribution.vertices.map(vertex => {
+        var matrix = new THREE.Matrix4();
+        matrix.lookAt(vertex, origin, up);
+        matrix.setPosition(vertex.clone().multiplyScalar(.75));
+        return matrix;
     });
-};
 
+    var specs = this.planetsSpec();
+
+    var instancedGeometryA = this.createInstancedGeometry(
+        wireframeBufferGeometry,
+        specs.wireframe,
+        snowflakeMatricies
+    );
+    var instancedMeshA = new THREE.Mesh(instancedGeometryA.geometry, materials.planetWireframe);
+    instancedMeshA.onBeforeRender = function() {
+        instancedGeometryA.updateRotation(this.app.elapsed * .00005);
+    }.bind(this);
+    this.group.add(instancedMeshA);
+
+    var instancedGeometryB = this.createInstancedGeometry(
+        bufferGeometry,
+        specs.solid,
+        snowflakeMatricies
+    );
+    var instancedMeshB = new THREE.Mesh(instancedGeometryB.geometry, materials.planetSolid);
+    instancedMeshB.onBeforeRender = function() {
+        instancedGeometryB.updateRotation(this.app.elapsed * .0001);
+    }.bind(this);
+    this.group.add(instancedMeshB);
+};
 
 Space.prototype.planetsSpec = function() {
 
@@ -184,7 +156,7 @@ Space.prototype.planetsSpec = function() {
         direction = new THREE.Vector3().fromArray(this.randomPointOnSphere(3, random.random));
         position = vert.clone().multiplyScalar(dist).add(direction);
         
-        rotateSpeed = THREE.Math.lerp(15000, 300000, size);
+        rotateSpeed = THREE.Math.lerp(1, .1, size);
 
         size = size * 6 + 1;
 
@@ -201,9 +173,15 @@ Space.prototype.planetsSpec = function() {
         );
 
         if (isSolid) {
-            specs.solid.push(matrix);
+            specs.solid.push({
+                matrix: matrix,
+                rotateSpeed: rotateSpeed
+            });
         } else {
-            specs.wireframe.push(matrix);
+            specs.wireframe.push({
+                matrix: matrix,
+                rotateSpeed: rotateSpeed
+            });
         }
     });
 
@@ -215,98 +193,59 @@ Space.prototype.createInstancedGeometry = function(bufferGeometry, parentSpecs, 
     instancedGeometry.attributes.position = bufferGeometry.attributes.position;
     instancedGeometry.attributes.normal = bufferGeometry.attributes.normal;
 
+    var children = childMatricies.length;
+
     var instancePositions = [];
     var instanceQuaternions = [];
     var instanceScales = [];
 
+    var matrix = new THREE.Matrix4();
     var position = new THREE.Vector3();
     var quaternion = new THREE.Quaternion();
     var scale = new THREE.Vector3();
 
     parentSpecs.forEach(spec => {
         childMatricies.forEach(child => {
-            var m = new THREE.Matrix4().multiplyMatrices(spec, child);
-            m.decompose(position, quaternion, scale);
+            matrix.multiplyMatrices(spec.matrix, child);
+            matrix.decompose(position, quaternion, scale);
             instancePositions.push(position.x, position.y, position.z);
             instanceQuaternions.push(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
             instanceScales.push(scale.x, scale.y, scale.z);
         });
     });
 
-    instancedGeometry.addAttribute('instancePosition', new THREE.InstancedBufferAttribute(new Float32Array(instancePositions), 3));
-    instancedGeometry.addAttribute('instanceQuaternion', new THREE.InstancedBufferAttribute(new Float32Array(instanceQuaternions), 4));
-    instancedGeometry.addAttribute('instanceScale', new THREE.InstancedBufferAttribute(new Float32Array(instanceScales), 3));
+    var positionAttr = new THREE.InstancedBufferAttribute(new Float32Array(instancePositions), 3).setDynamic(true);
+    var quaternionAttr = new THREE.InstancedBufferAttribute(new Float32Array(instanceQuaternions), 4).setDynamic(true);
+    var scaleAttr = new THREE.InstancedBufferAttribute(new Float32Array(instanceScales), 3);
 
-    return instancedGeometry;
-};
+    instancedGeometry.addAttribute('instancePosition', positionAttr);
+    instancedGeometry.addAttribute('instanceQuaternion', quaternionAttr);
+    instancedGeometry.addAttribute('instanceScale', scaleAttr);
 
-Space.prototype.test = function() {
+    var rotationA = new THREE.Matrix4();
+    var rotationB = new THREE.Matrix4();
 
-    var bufferGeometry = new THREE.OctahedronBufferGeometry();
-    bufferGeometry.scale(.2,.2,.5);
-
-    var wireframeGeometry = new THREE.OctahedronGeometry();
-    wireframeGeometry.scale(.2,.2,.5);
-    wireframeGeometry = geometryTools.wireframeMesh(wireframeGeometry, .001);
-    var wireframeBufferGeometry = new THREE.BufferGeometry().fromGeometry(wireframeGeometry);
-
-    var origin = new THREE.Vector3();
-    var up = new THREE.Vector3(0,1,0);
-
-    var icoDistribution = new THREE.IcosahedronGeometry(1);
-
-    var snowflakeMatricies = icoDistribution.vertices.map(vertex => {
-        var matrix = new THREE.Matrix4();
-        matrix.lookAt(vertex, origin, up);
-        matrix.setPosition(vertex.clone().multiplyScalar(.75));
-        return matrix;
-    });
-
-    var specs = this.planetsSpec();
-
-    var instancedGeometryA = this.createInstancedGeometry(
-        wireframeBufferGeometry,
-        specs.wireframe,
-        snowflakeMatricies
-    );
-    var instancedMeshA = new THREE.Mesh(instancedGeometryA, materials.planetWireframe);
-    this.group.add(instancedMeshA);
-
-    var instancedGeometryB = this.createInstancedGeometry(
-        bufferGeometry,
-        specs.solid,
-        snowflakeMatricies
-    );
-    var instancedMeshB = new THREE.Mesh(instancedGeometryB, materials.planetSolid);
-    this.group.add(instancedMeshB);
-};
-
-Space.prototype.geometries = function() {
-
-    var geometry = new THREE.Geometry();
-
-    var shard = new THREE.OctahedronGeometry();
-    shard.scale(.2,.2,.5);
-
-    var distribution = new THREE.IcosahedronGeometry(1);
-    distribution.vertices.forEach(vertex => {
-        var crystal = shard.clone();
-        crystal.translate(0,0,1);
-        crystal.lookAt(vertex);
-        geometry.merge(crystal);
-    });
-
-
-    return [
-        geometry,
-        geometry,
-        geometry
-    ].map(geometry => {
-        return {
-            solid: geometry,
-            wireframe: new THREE.EdgesGeometry(geometry)
-        };
-    });
+    return {
+        geometry: instancedGeometry,
+        updateRotation: function(d) {
+            parentSpecs.forEach((spec, p) => {
+                rotationA.makeRotationZ(d * spec.rotateSpeed);
+                rotationB.makeRotationZ(d * spec.rotateSpeed * 3);
+                childMatricies.forEach((child, c) => {
+                    var i = p * children + c;
+                    matrix.copy(spec.matrix);
+                    matrix.multiply(rotationA);
+                    matrix.multiply(child);
+                    matrix.multiply(rotationB);
+                    matrix.decompose(position, quaternion, scale);
+                    positionAttr.setXYZ(i, position.x, position.y, position.z);
+                    quaternionAttr.setXYZW(i, quaternion.x, quaternion.y, quaternion.z, quaternion.w);
+                });
+            });
+            positionAttr.needsUpdate = true;
+            quaternionAttr.needsUpdate = true;
+        }
+    };
 };
 
 Space.prototype.randomPointOnSphere = function(radius, rand) {
