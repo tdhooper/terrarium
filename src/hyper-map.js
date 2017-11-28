6df3ece9693883987877b8763f7b00ca621995fd
Thomas Hooper <thomash@last.fm>
2017-11-28 23:20:53 +0000

    Awful hack to keep hyper on when clicking fast

--- src/hyper-map.js
+++ src/hyper-map.js
@@ -7,23 +7,51 @@ const HyperMap = function(easing) {
     this.waveDuration = 3; // seconds
     this.wavePower = .75;
     this.dataTexture = [0,0,0,0];
+    this.tapSpeed = 0;
+    this.lastTap = 0;
 };
 
 HyperMap.prototype.addWave = function() {
+    var tap = Date.now();
+    var delta = tap - this.lastTap;
+    this.tapSpeed = Math.max(0, 500 - delta) / 500;
+    this.lastTap = tap;
     if (this.waves.length < this.dataTexture.length) {
         this.waves.unshift(0);
     }
 };
 
 HyperMap.prototype.update = function(delta) {
-    // return;
     delta /= this.waveDuration;
+    var fullOn = 0;
     this.waves = this.waves
-        .map(wave => wave + delta)
+        .map(wave => {
+            if (
+                this.tapSpeed > .5 &&
+                wave > .2 &&
+                wave < .35
+            ) {
+                return .25;
+            }
+            return wave + delta;
+        })
+        .filter(wave => {
+            if (
+                wave > .2 &&
+                wave < .35
+            ) {
+                fullOn += 1;
+                if (fullOn > 2) {
+                    return false;
+                }
+            }
+            return true;
+        })
         .filter(wave => wave <= 1);
     this.dataTexture.forEach((v, i) => {
         this.dataTexture[i] = i < this.waves.length ? this.waves[i] : 0;
     });
+    this.tapSpeed *= .9;
 };
 
 
